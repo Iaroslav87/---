f8316089a4eadccc564e4de5552a593058f04bca
Iaroslav87 <91834197+Iaroslav87@users.noreply.github.com>
2022-02-27 22:04:52 +0300

    Lesson 2
    Lesson 2 HW

--- EgorovIaroslav_HW_2_1_GB(05102021).py
+++ EgorovIaroslav_HW_2_1_GB(05102021).py
@@ -1,17 +1,47 @@
-#1. Выяснить тип результата выражений:
-#15 * 3
-#15 / 3
-#15 // 2
-#15 ** 2
+"""
+1. Написать программу, которая будет складывать, вычитать, умножать или делить два числа. 
+Числа и знак операции вводятся пользователем. После выполнения вычисления программа не должна завершаться, 
+а должна запрашивать новые данные для вычислений. Завершение программы должно выполняться при вводе 
+символа '0' в качестве знака операции. Если пользователь вводит неверный знак (не '0', '+', '-', '*', '/'), 
+то программа должна сообщать ему об ошибке и снова запрашивать знак операции. Также сообщать пользователю 
+о невозможности деления на ноль, если он ввел 0 в качестве делителя
+"""
 
-a = 15 * 3
-print(a)
-b = 15 / 3
-print(b)
-c = 15 // 2
-print(c)
-d = 15 ** 2
-print(d)
-expression_type = ['a','b','c','d']
-print(type(expression_type) == list)
-print(type(expression_type) == int)
+def calculate(a, b, operator):
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == '*':
+        return a * b
+    else:
+        return a / b
+
+def is_correct_operator(operator):
+    return operator == '0' or operator == '+' or operator == '-' or operator == '*' or operator == '/'
+
+def get_operator():
+    return input('operator: ')
+
+def run_calculator(a=None, b=None):
+    if a is None:
+        a = float(input('a: '))
+    if b is None:
+        b = float(input('b: '))
+    operator = get_operator()
+
+    if not is_correct_operator(operator):
+        print('Wrong operator')
+        run_calculator(a, b)
+        return
+    elif operator == '0':
+        return
+    elif operator == '/' and b == 0:
+        print('No division by zero')
+        run_calculator()
+        return
+    else:
+        print(calculate(a, b, operator))
+        run_calculator()
+
+run_calculator()
