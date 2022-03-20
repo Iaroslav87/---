3ef18d919dff4040fadafe66f893d991b3cb9c09
Iaroslav87 <91834197+Iaroslav87@users.noreply.github.com>
2022-03-20 22:27:19 +0300

    Lesson5
    Lesson5HW

--- EgorovIaroslav_HW_5_2_GB(05102021).py
+++ EgorovIaroslav_HW_5_2_GB(05102021).py
@@ -1,7 +1,7 @@
-"""
-2. * (вместо 1) Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield.
-"""
+a = list(input("Введите первое число"))
+b = list(input("Введите второе число"))
 
-max_val = 3
-odd_nums_gen = (n for n in range(1, max_val + 1, 2))
-print(next(odd_nums_gen))
+suma = hex(int(''.join(a), 16) + int(''.join(b), 16)).split('0x')[1]
+multiplication = hex(int(''.join(a), 16) * int(''.join(b), 16)).split('0x')[1]
+
+print(suma, multiplication)
